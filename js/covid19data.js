378344dda2bf7d8f998b79fa71fb9bd64dcf22ea
dhachanamoorthy <dhachanamoorthy3@gmail.com>
2020-08-28 08:30:14 +0530

    covid -19 -data updated

--- js/covid19data.js
+++ js/covid19data.js
@@ -13,39 +13,45 @@ $(function() {
                 array.push(d.TotalRecovered);
                 array.push(d.NewConfirmed);
                regionData[a]=array;
+               if(d.Country=='India'){
+                $('#active_data').append(d.NewConfirmed);
+                $('#recovered').append(d.TotalRecovered);
+                $('#total').append(d.TotalConfirmed);
+                $('#death').append(d.TotalDeaths);
+               }
             }
         })
     }
-    jQuery('#world-map-markers').vectorMap({
-        map: 'world_mill_en',
-        backgroundColor: 'transparent',
-        borderColor: '#818181',
-        borderOpacity: 0.25,
-        borderWidth: 1,
-        zoomOnScroll: false,
+    // jQuery('#world-map-markers').vectorMap({
+    //     map: 'world_mill_en',
+    //     backgroundColor: 'transparent',
+    //     borderColor: '#818181',
+    //     borderOpacity: 0.25,
+    //     borderWidth: 1,
+    //     zoomOnScroll: false,
         
-        regionStyle: {
-            initial: {
-                fill: ['#0093BE'],
-                scale: ['#fff','#FF0000']
-            }
-        },
-        enableZoom: true,
-        hoverColor: '#8b94d6',
-        hoverOpacity: null,
-        normalizeFunction: 'linear',
+    //     regionStyle: {
+    //         initial: {
+    //             fill: ['#0093BE'],
+    //             scale: ['#fff','#FF0000']
+    //         }
+    //     },
+    //     enableZoom: true,
+    //     hoverColor: '#8b94d6',
+    //     hoverOpacity: null,
+    //     normalizeFunction: 'linear',
         
-        series:{
-            regions: [{
-                values: regionData,
-                scale: ['#fff','#FF0000'],
-                normalizeFunction: 'polynomial'
-            }]
-        },
-        onRegionTipShow: function(e, el, code){
-            el.html('<h5 id="country">'+el.html()+'</h5><h5 id="red">Total Cases - '+regionData[code][0]+'</h5><h5 id="green">Total Recovered-'+regionData[code][1]+'</h5><h5 id="yellow">New Cases -'+regionData[code][2]+'</h5>');
-        }
-    });
+    //     series:{
+    //         regions: [{
+    //             values: regionData,
+    //             scale: ['#fff','#FF0000'],
+    //             normalizeFunction: 'polynomial'
+    //         }]
+    //     },
+    //     onRegionTipShow: function(e, el, code){
+    //         el.html('<h5 id="country">'+el.html()+'</h5><h5 id="red">Total Cases - '+regionData[code][0]+'</h5><h5 id="green">Total Recovered-'+regionData[code][1]+'</h5><h5 id="yellow">New Cases -'+regionData[code][2]+'</h5>');
+    //     }
+    // });
    
     // $('#world-map-gdp').vectorMap({
     //     map: 'world_mill_en',
