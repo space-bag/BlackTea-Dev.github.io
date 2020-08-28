18cb316480ac68d7042ee60b33244a985da369b1
dhachanamoorthy <dhachanamoorthy3@gmail.com>
2020-08-28 16:19:28 +0530

    changed covid data api

--- js/covid19data.js
+++ js/covid19data.js
@@ -1,27 +1,44 @@
 $(function() {
     "use strict";
     var regionData={};
-    loadData();
-    function loadData(){
-        var api="https://api.covid19api.com/summary";
-        $.get(api,function(data){
-            data.Countries.forEach(getData);
-            function getData(d){
-                var a=d.CountryCode;
-                var array=[];
-                array.push(d.TotalConfirmed);
-                array.push(d.TotalRecovered);
-                array.push(d.NewConfirmed);
-               regionData[a]=array;
-               if(d.Country=='India'){
-                $('#active_data').append(d.NewConfirmed);
-                $('#recovered').append(d.TotalRecovered);
-                $('#total').append(d.TotalConfirmed);
-                $('#death').append(d.TotalDeaths);
-               }
-            }
-        })
-    }
+    // loadData();
+    // function loadData(){
+    //     var api="https://api.covid19api.com/summary";
+    //     $.get(api,function(data){
+    //         data.Countries.forEach(getData);
+    //         function getData(d){
+    //             var a=d.CountryCode;
+    //             var array=[];
+    //             array.push(d.TotalConfirmed);
+    //             array.push(d.TotalRecovered);
+    //             array.push(d.NewConfirmed);
+    //            regionData[a]=array;
+    //            if(d.Country=='India'){
+    //             $('#active_data').append(d.NewConfirmed);
+    //             $('#recovered').append(d.TotalRecovered);
+    //             $('#total').append(d.TotalConfirmed);
+    //             $('#death').append(d.TotalDeaths);
+    //            }
+    //         }
+    //     })
+    // }
+
+    $.getJSON("https://api.covid19india.org/data.json", function (data) {
+
+
+        let total_active = data.statewise[0].active;
+        let total_confirmed = data.statewise[0].confirmed;
+        let total_recovered = data.statewise[0].recovered;
+        let total_deaths = data.statewise[0].deaths;
+        let last_update = data.statewise[0].lastupdatedtime;
+
+        console.log(data.statewise[0].lastupdatedtime);
+        $("#lastupdated").append(last_update);
+        $('#active_data').append(total_active);
+        $('#recovered').append(total_recovered);
+        $('#total').append(total_confirmed);
+        $('#death').append(total_deaths);
+});
     // jQuery('#world-map-markers').vectorMap({
     //     map: 'world_mill_en',
     //     backgroundColor: 'transparent',
